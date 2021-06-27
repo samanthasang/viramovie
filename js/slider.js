d6e0f4e349d6461c3ac6a33d2eb05ad889aed76b
majidak <majid.ebrahimi989@gmail.com>
2021-06-27 21:58:37 +0430

    add all file again
    add new file to old project

---
+++ js/slider.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.CenterA').slick({
+        centerMode: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow:'#PRV',
+        nextArrow:'#NTX',
+    });
+
+});
